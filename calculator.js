1b4554d3375f2161ac252de25c06d8c3d3721736
Filip <filipfrenchowicz@gmail.com>
2023-06-15 20:40:56 +0200

    first try on cost calculator, big main site update

---
+++ calculator.js
@@ -0,0 +1,8 @@
+function getValue(){
+     option1 = document.getElementById("calc_value").value;
+     option2 = document.getElementById("calc_value2").value;
+    var sum;
+    sum = Number(calc_value.value) + Number(calc_value2.value);
+    console.log(sum);
+
+}
