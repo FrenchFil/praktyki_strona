77a41d39ce65163331906f15f97377de40bacd77
Filip <filipfrenchowicz@gmail.com>
2023-06-28 22:20:28 +0200

    another calculator

---
+++ calculator2.js
@@ -0,0 +1,10 @@
+function getValue(){
+    option1 = document.getElementById("calc_value").value;
+   var sum;
+   if(option1 == 0){
+       
+      }
+   sum = Number(calc_value.value)*140;
+   console.log(sum);
+   document.getElementById('calc_result').value = sum;
+}
