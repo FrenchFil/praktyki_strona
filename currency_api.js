be1674de127f3510a673b72eba5e4b4cf3545a4b
FrenchFil <filipfrenchowicz@gmail.com>
2024-03-18 10:17:50 +0100

    porzadki

--- currency_api.js
+++
@@ -1,9 +0,0 @@
-const host = 'api.frankfurter.app';
-fetch(`https://${host}/latest`)
-  .then(resp => resp.json())
-  .then((data) => {
-   // alert(`1 PLN = ${data.rates.PLN} EURO`);
-    var currency = data.rates.PLN;
-    let test = document.getElementById('span');
-    document.getElementById('span').innerHTML = currency + " pln to euro";
-  });
